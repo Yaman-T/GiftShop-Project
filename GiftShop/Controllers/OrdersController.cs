0aadf0c72672bc252d9836eeab5f70a91e931848
milla1114 <millionmesfind@gmail.com>
2022-10-01 21:45:29 +0300

    Order and shopping cart implemented

---
+++ GiftShop/Controllers/OrdersController.cs
@@ -0,0 +1,79 @@
+﻿using GiftShop.Data.Cart;
+using GiftShop.Data.Services;
+using GiftShop.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftShop.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IItemsService _itemsService;
+        private readonly ShoppingCart _shoppingCart;
+        private readonly IOrdersService _ordersService;
+
+        public OrdersController(IItemsService itemsService, ShoppingCart shoppingCart, IOrdersService ordersService)
+        {
+            _itemsService = itemsService;
+            _shoppingCart = shoppingCart;
+            _ordersService = ordersService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
+            var orders = await _ordersService.GetOrdersByUserId(userId);
+            return View(orders);
+        }
+
+        public IActionResult ShoppingCart()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+
+        public async Task<IActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _itemsService.GetItemById(id);
+
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _itemsService.GetItemById(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> CompleteOrder()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            string userId = "";
+            string userEmailAddress = "";
+
+            await _ordersService.StoreOrder(items, userId, userEmailAddress);
+            await _shoppingCart.ClearShoppingCart();
+            return View("OrderCompleted");
+        }
+    }
+}
